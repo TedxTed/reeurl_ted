407a6daa09b27b4c3b413119dc3e7d140259fcee
Ted <wodf410@gmail.com>
2022-10-05 15:06:37 +0800

    rspec and pgsql

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { 
+    described_class.new(password: "some_password",
+                        email: "john@doe.com")}
+
+  
+  context "when user create" do 
+    it "create user " do 
+      expect(subject).to eq(User.last)
+    end
+  end 
 end
