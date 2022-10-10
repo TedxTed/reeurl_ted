cfd817da6a1d781d6709b7d63377b8f3d50e64fe
Ted <wodf410@gmail.com>
2022-10-10 23:18:34 +0800

    clean home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,3 @@
 class HomeController < ApplicationController
-  def index
-    @current_user = current_user
-    @links = Link.find_by(user_id: @current_user.id) if @current_user
-  end
+  def index; end
 end
